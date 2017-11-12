bfc152460cce1399691ab0cfd64c56a289560684
Robin Dang <dang.robin@gmail.com>
2017-11-12 01:35:15 -0500

    cookie cutter template

--- controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .api import *
-from .main import *
-from .controller_utils import *
-from .admin import *
-from .user import *
--- models/user.py
+++
@@ -1,5 +0,0 @@
-from weppy.tools.auth.models import AuthUser
-
-
-class User(AuthUser):
-    pass
